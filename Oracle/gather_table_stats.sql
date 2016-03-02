e7434803d391b09ef2c79dd8ad5cfaeeaeeb236e
Nathan Graham <ngraham@kumc.edu>
2016-03-02 12:20:22 -0600

    Avoid selecting from dba_tables as it requires elevated privileges.

--- Oracle/gather_table_stats.sql
+++ Oracle/gather_table_stats.sql
@@ -1,14 +1,18 @@
 /* Metadata table statistics - experimentation shows vastly better performance
 if this is done before the CDM transform.
+
+ACK: http://stackoverflow.com/questions/2242024/for-each-string-execute-a-function-procedure
 */
+declare
+table_list sys.dbms_debug_vc2coll := sys.dbms_debug_vc2coll(
+  'PCORNET_DEMO', 'PCORNET_DIAG', 'PCORNET_DIAG_BACKUP', 'PCORNET_ENC', 
+  'PCORNET_ENROLL', 'PCORNET_LAB', 'PCORNET_MED', 'PCORNET_PROC', 'PCORNET_VITAL');
 begin 
-  for rec in (select table_name 
-              from dba_tables 
-              where owner = '&&i2b2_meta_schema' and table_name like 'PCORNET%')
+  for t in table_list.first .. table_list.last
   loop
     DBMS_STATS.GATHER_TABLE_STATS (
             ownname => '"&&i2b2_meta_schema"',
-            tabname => rec.table_name,
+            tabname => table_list(t),
             estimate_percent => 40
             );
   end loop;
