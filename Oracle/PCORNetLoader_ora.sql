e4c43961c3e9eb43ed6bbd199cabb3373f74256f
Nathan Graham <ngraham@kumc.edu>
2016-03-08 09:54:06 -0600

    Hard-code px_source as "billing" for now.

--- Oracle/PCORNetLoader_ora.sql
+++ Oracle/PCORNetLoader_ora.sql
@@ -1126,9 +1126,11 @@ end PCORNetCondition;
 create or replace procedure PCORNetProcedure as
 begin
 insert into procedures( 
-				patid,			encounterid,	enc_type, admit_date, px_date, providerid, px, px_type) 
+				patid,			encounterid,	enc_type, admit_date, px_date, providerid, px, px_type, px_source) 
 select  distinct fact.patient_num, enc.encounterid,	enc.enc_type, enc.admit_date, fact.start_date, 
-		fact.provider_id, SUBSTR(pr.pcori_basecode,INSTR(pr.pcori_basecode, ':')+1,11) px, SUBSTR(pr.c_fullname,18,2) pxtype 
+		fact.provider_id, SUBSTR(pr.pcori_basecode,INSTR(pr.pcori_basecode, ':')+1,11) px, SUBSTR(pr.c_fullname,18,2) pxtype,
+    -- All are billing for now - see https://informatics.gpcnetwork.org/trac/Project/ticket/491
+    'BI' px_source
 from i2b2fact fact
  inner join encounter enc on enc.patid = fact.patient_num and enc.encounterid = fact.encounter_Num
  inner join	pcornet_proc pr on pr.c_basecode  = fact.concept_cd   
