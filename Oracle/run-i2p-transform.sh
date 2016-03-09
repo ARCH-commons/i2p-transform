53001c8da12872c4ac6393a45004aab93c9e2d30
Nathan Graham <ngraham@kumc.edu>
2016-03-09 16:38:00 -0600

    Load local mapping tables before testing for ethnicity_cd, etc.
     - This is helpful because the mapping table is used in the SQL to update ethnicity_cd.

--- Oracle/run-i2p-transform.sh
+++ Oracle/run-i2p-transform.sh
@@ -19,6 +19,26 @@ set -e
 # All i2b2 terms - used for local path mapping
 #export terms_table=
 
+# Create/Load the local path mapping and ontology update tables
+sqlplus /nolog <<EOF
+connect ${pcornet_cdm_user}/${pcornet_cdm}
+
+set echo on;
+
+WHENEVER SQLERROR CONTINUE;
+drop table pcornet_mapping;
+drop table pcornet_ontology_updates;
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+
+start pcornet_mapping_ddl.sql
+start pcornet_ontology_updates_ddl.sql
+EOF
+
+ORACLE_SID=${sid} sqlldr ${pcornet_cdm_user}/${pcornet_cdm} control=pcornet_mapping.ctl data=pcornet_mapping.csv bad=pcornet_mapping.bad log=pcornet_mapping.log errors=0
+
+ORACLE_SID=${sid} sqlldr ${pcornet_cdm_user}/${pcornet_cdm} control=pcornet_ontology_updates.ctl data=pcornet_ontology_updates.csv bad=pcornet_ontology_updates.bad log=pcornet_ontology_updates.log errors=0
+
+# Run some tests
 sqlplus /nolog <<EOF
 connect ${pcornet_cdm_user}/${pcornet_cdm}
 
@@ -40,24 +60,6 @@ select case when qty = 0 then 1/0 else 1 end inout_cd_populated from (
 
 EOF
 
-# Create/Load the local path mapping and ontology update tables
-sqlplus /nolog <<EOF
-connect ${pcornet_cdm_user}/${pcornet_cdm}
-
-set echo on;
-
-WHENEVER SQLERROR CONTINUE;
-drop table pcornet_mapping;
-drop table pcornet_ontology_updates;
-WHENEVER SQLERROR EXIT SQL.SQLCODE;
-
-start pcornet_mapping_ddl.sql
-start pcornet_ontology_updates_ddl.sql
-EOF
-
-ORACLE_SID=${sid} sqlldr ${pcornet_cdm_user}/${pcornet_cdm} control=pcornet_mapping.ctl data=pcornet_mapping.csv bad=pcornet_mapping.bad log=pcornet_mapping.log errors=0
-
-ORACLE_SID=${sid} sqlldr ${pcornet_cdm_user}/${pcornet_cdm} control=pcornet_ontology_updates.ctl data=pcornet_ontology_updates.csv bad=pcornet_ontology_updates.bad log=pcornet_ontology_updates.log errors=0
 
 # Insert local terms as leaves of the PCORNet terms and run the transform
 sqlplus /nolog <<EOF
