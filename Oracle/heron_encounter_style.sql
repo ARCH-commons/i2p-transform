0cc5ea6c4429f1ce1eb8a64b07b52f70cad483cc
Dan Connolly <dconnolly@kumc.edu>
2016-03-15 14:22:22 -0500

    convert inout_cd update to use `merge into ...`
    move manual update of `encounter` after inout_cd merge

--- Oracle/heron_encounter_style.sql
+++ Oracle/heron_encounter_style.sql
@@ -76,16 +76,6 @@ when matched then update
 -- 3,879,931 rows merged.
 
 
-/* -- Manually update encounter (i2p-transform should fill in the column)
-merge into encounter pe
-using "&&i2b2_data_schema".visit_dimension vd
-   on ( pe.encounterid = vd.encounter_num
-        and vd.end_date is not null)
-when matched then update set pe.discharge_date = vd.end_date;
--- 165,292 rows merged.
-*/
-
-
 /* TODO: get encounter type from source system? */
 /* TODO: Consider moving to cdm_transform_tests.sql
 select count(*), sourcesystem_cd
@@ -136,36 +126,47 @@ order by 1
 ;
 
 
-create table enc_type as
-with
-enc_type_codes as (
-  select 
-    pm.pcori_path, replace(substr(pm.pcori_path, instr(pm.pcori_path, '\', -2)), '\', '') pcori_code, pm.local_path, cd.concept_cd
+merge into "&&i2b2_data_schema".visit_dimension vd
+using (
+  with
+  enc_type_codes as (
+    select pm.pcori_path
+         , replace(substr(pm.pcori_path, instr(pm.pcori_path, '\', -2)), '\', '') pcori_code
+         , pm.local_path
+         , cd.concept_cd
   from pcornet_mapping pm 
   join "&&i2b2_data_schema".concept_dimension cd on cd.concept_path like pm.local_path || '%'
   where pm.pcori_path like '\PCORI\ENCOUNTER\ENC_TYPE\%'
   )
--- TODO: Consider investigating why we have multiple encounter types for a single encounter
+-- Note: our patient-day style encounters etc. may result in
+-- multiple encounter types for a single encounter.
 select obs.encounter_num, max(et.pcori_code) pcori_code
 from "&&i2b2_data_schema".observation_fact obs
 join enc_type_codes et on et.concept_cd = obs.concept_cd
 group by obs.encounter_num
-;
+) et on ( vd.encounter_num = et.encounter_num )
+when matched then update
+set inout_cd = et.pcori_code;
+-- 11,085,911 rows merged.
 
-create index enc_type_codes_encnum_idx on enc_type(encounter_num);
 
-update "&&i2b2_data_schema".visit_dimension vd
-set inout_cd = coalesce((
-  select et.pcori_code from enc_type et
-  where vd.encounter_num = et.encounter_num
-  ), 'UN');
+/* -- Manually update encounter (i2p-transform should fill in the column)
 
--- Make sure the update went as expected
-select case when count(*) > 0 then 1/0 else 1 end inout_cd_update_match from (
-  select * from (
-    select vd.inout_cd vd_code, et.pcori_code et_code
-    from "&&i2b2_data_schema".visit_dimension vd
-    left join enc_type et on et.encounter_num = vd.encounter_num
-    )
-  where vd_code != vd_code and not (vd_code = 'UN' and vd_code is null)
-  );
+select count(*) from encounter;
+-- 14,665,341
+
+merge into encounter pe
+using "&&i2b2_data_schema".visit_dimension vd
+   on ( pe.encounterid = vd.encounter_num
+        and vd.end_date is not null)
+when matched then update set pe.discharge_date = vd.end_date;
+-- 165,292 rows merged.
+
+merge into encounter pe
+using "&&i2b2_data_schema".visit_dimension vd
+   on ( pe.encounterid = vd.encounter_num
+        and vd.inout_cd is not null)
+when matched then update
+ set pe.enc_type = vd.inout_cd;
+ -- 14,665,341 rows merged.
+*/
