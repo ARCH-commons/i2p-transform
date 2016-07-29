0a7dce08a6027e8625a32e40acce55e51ecbfc67
Michael Prittie <mprittie@kumc.edu>
2016-07-29 15:41:48 -0500

    Added PATID and ENCOUNTERID indexes to tables that contain these fields.

--- Oracle/PCORNetLoader_ora.sql
+++ Oracle/PCORNetLoader_ora.sql
@@ -873,6 +873,9 @@ FETCH getsql INTO sqltext;
 	COMMIT;
 END LOOP;
 CLOSE getsql;
+
+execute immediate 'create index demographic_patid on pcornet_cdm.demographic (PATID)';
+
 end PCORNetDemographic; 
 /
 
@@ -923,6 +926,9 @@ left outer join
 (select patient_num, encounter_num, inout_cd,SUBSTR(pcori_basecode,INSTR(pcori_basecode, ':')+1,2) pcori_enctype from i2b2visit v
  inner join pcornet_enc e on c_dimcode like '%'''||inout_cd||'''%' and e.c_fullname like '\PCORI\ENCOUNTER\ENC_TYPE\%') enctype
   on enctype.patient_num=v.patient_num and enctype.encounter_num=v.encounter_num;
+
+execute immediate 'create index encounter_patid on encounter (PATID)';
+execute immediate 'create index encounter_encounterid on encounter (ENCOUNTERID)';
 
 end PCORNetEncounter;
 /
@@ -981,6 +987,8 @@ sqltext := 'insert into diagnosis (patid,			encounterid,	enc_type, admit_date, p
 
 PMN_EXECUATESQL(sqltext);
 
+execute immediate 'create index diagnosis_patid on diagnosis (PATID)';
+execute immediate 'create index diagnosis_encounterid on diagnosis (ENCOUNTERID)';
 
 end PCORNetDiagnosis;
 /
@@ -1024,6 +1032,9 @@ sqltext := 'insert into condition (patid, encounterid, report_date, resolve_date
 
 PMN_EXECUATESQL(sqltext);
 
+execute immediate 'create index condition_patid on condition (PATID)';
+execute immediate 'create index condition_encounterid on condition (ENCOUNTERID)';
+
 end PCORNetCondition;
 /
 
@@ -1045,6 +1056,9 @@ from i2b2fact fact
  inner join encounter enc on enc.patid = fact.patient_num and enc.encounterid = fact.encounter_Num
  inner join	pcornet_proc pr on pr.c_basecode  = fact.concept_cd   
 where pr.c_fullname like '\PCORI\PROCEDURE\%';
+
+execute immediate 'create index procedures_patid on procedures (PATID)';
+execute immediate 'create index procedures_encounterid on procedures (ENCOUNTERID)';
 
 end PCORNetProcedure;
 /
@@ -1130,6 +1144,9 @@ where ht is not null
   or tobacco is not null
 group by patid, encounterid, measure_date, measure_time, admit_date) y;
 
+execute immediate 'create index vital_patid on vital (PATID)';
+execute immediate 'create index vital_encounterid on vital (ENCOUNTERID)';
+
 end PCORNetVital;
 /
 
@@ -1161,6 +1178,7 @@ from enrolled enr
 join i2b2visit visit on enr.patient_num = visit.patient_num
 group by visit.patient_num;
 
+execute immediate 'create index enrollment_patid on enrollment (PATID)';
 
 end PCORNetEnroll;
 /
@@ -1321,6 +1339,9 @@ and M.start_date=l.start_Date
 WHERE m.ValType_Cd in ('N','T')
 and ont_parent.C_BASECODE LIKE 'LAB_NAME%' -- Exclude non-pcori labs
 and m.MODIFIER_CD='@';
+
+execute immediate 'create index lab_result_cm_patid on lab_result_cm (PATID)';
+execute immediate 'create index lab_result_cm_encounterid on lab_result_cm (ENCOUNTERID)';
 
 END PCORNetLabResultCM;
 /
@@ -1499,6 +1520,8 @@ inner join encounter enc on enc.encounterid = m.encounter_Num
 
 where (basis.c_fullname is null or basis.c_fullname like '\PCORI_MOD\RX_BASIS\PR\%');
 
+execute immediate 'create index prescribing_patid on prescribing (PATID)';
+execute immediate 'create index prescribing_encounterid on prescribing (ENCOUNTERID)';
 
 end PCORNetPrescribing;
 /
@@ -1583,6 +1606,8 @@ inner join encounter enc on enc.encounterid = m.encounter_Num
     and m.concept_cd = amount.concept_Cd
 
 group by m.encounter_num ,m.patient_num, m.start_date,  mo.pcori_ndc;
+
+execute immediate 'create index dispensing_patid on dispensing (PATID)';
 
 end PCORNetDispensing;
 /
