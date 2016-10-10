0e0da8c3f3c2961c94cae341944ffdd9c6e5b921
Matthew Joss <mjoss@partners.org>
2016-10-10 15:27:01 -0400

    added a new run script to the oracle transform

--- Oracle/PCORNetLoader_ora.sql
+++ Oracle/PCORNetLoader_ora.sql
@@ -91,25 +91,17 @@ CREATE OR REPLACE SYNONYM I2B2FACT FOR I2B2DEMODATA.OBSERVATION_FACT
 /
 
 
-BEGIN
-PMN_DROPSQL('DROP TABLE i2b2patient_list');
-END;
-/
+-- extracted i2b2patient_list create table statement and inserted into the new run script. MJ 10/10/16
 
-CREATE table i2b2patient_list as 
-select * from
-(
-select DISTINCT f.PATIENT_NUM from I2B2FACT f 
-inner join i2b2visit v on f.patient_num=v.patient_num
-where f.START_DATE >= to_date('01-Jan-2010','dd-mon-rrrr') and v.START_DATE >= to_date('01-Jan-2010','dd-mon-rrrr')
-) where ROWNUM<100000000
-/
+
 
 create or replace VIEW i2b2patient as select * from I2B2DEMODATA.PATIENT_DIMENSION where PATIENT_NUM in (select PATIENT_NUM from i2b2patient_list)
 /
 
 create or replace view i2b2visit as select * from I2B2DEMODATA.VISIT_DIMENSION where START_DATE >= to_date('01-Jan-2010','dd-mon-rrrr') and (END_DATE is NULL or END_DATE < CURRENT_DATE) and (START_DATE <CURRENT_DATE)
 /
+
+
 
 
 CREATE OR REPLACE SYNONYM pcornet_med FOR  i2b2metadata.pcornet_med
@@ -2187,9 +2179,4 @@ PCORNetDeath;
 end pcornetloader;
 /
 
-
-BEGIN          -- RUN PROGRAM 
-pcornetclear;  -- Make sure to run this before re-populating any pmn tables.
-pcornetloader; -- you may want to run sql statements one by one in the pcornetloader procedure :)
-END;
-/
+--new run script executes pcornet clear and pcornet loader. 
