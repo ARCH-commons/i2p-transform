e09eefe47eee4e765597e5ee5a2c194a9180af3f
Dan Connolly <dconnolly@kumc.edu>
2016-12-06 09:51:42 -0600

    PCORNetDeath from SCILHS
    upstream commit: d2681db Sep 1

--- Oracle/PCORNetLoader_ora.sql
+++ Oracle/PCORNetLoader_ora.sql
@@ -1804,9 +1804,31 @@ end PCORNetDispensing;
 /
 
 
+----------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------
+-- 11. Death - by Jeff Klann, PhD
+-- Simple transform only pulls death date from patient dimension, does not rely on an ontology
+-- Translated from MSSQL script by Matthew Joss
+----------------------------------------------------------------------------------------------------------------------------------------
 
 
 
+create or replace procedure PCORNetDeath as
+
+begin
+
+insert into pmndeath( patid, death_date, death_date_impute, death_source, death_match_confidence) 
+select  distinct pat.patient_num, pat.death_date, case 
+when vital_status_cd like 'X%' then 'B'
+when vital_status_cd like 'M%' then 'D'
+when vital_status_cd like 'Y%' then 'N'
+else 'OT'	
+end, 'NI','NI'
+from i2b2patient pat
+where (pat.death_date is not null or vital_status_cd like 'Z%') and pat.patient_num in (select patid from pmndemographic);
+
+end;
+/
 
 
 create or replace PROCEDURE pcornetReport
@@ -1906,6 +1928,7 @@ http://listserv.kumc.edu/pipermail/gpc-dev/attachments/20160223/8d79fa70/attachm
 > what we have in our i2b2
 */
 PCORNetDispensing;
+PCORNetDeath;
 PCORNetHarvest;
 
 end pcornetloader;
