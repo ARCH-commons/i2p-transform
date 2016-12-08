96c18223a071b4c1c08f3bc47068771b70c890d0
Michael Prittie <mprittie@kumc.edu>
2016-12-08 14:03:57 -0600

    Removed hard-coded refs

--- Oracle/observation_fact_meds.sql
+++ Oracle/observation_fact_meds.sql
@@ -84,7 +84,7 @@ drop table med_order_instance_map;
 create table med_order_instance_map as 
 with id_med_instance as (
   select instance_num
-  from nightherondata.observation_fact@id
+  from "&&i2b2_id_data_schema".observation_fact"&&i2b2_id_data_link"
   where concept_cd like 'KUH|MEDICATION_ID:%'
 ),
 -- based on instance_num deid in i2b2_facts_deid.sql.
