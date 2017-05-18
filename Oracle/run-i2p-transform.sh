d4aa672acbba11b8e0e793ee39d98e173e884d84
Michael Prittie <mprittie@kumc.edu>
2017-05-18 10:03:32 -0500

    Updated run shell script to reflect changes to PCORNetLoader and PCORNetInit

--- Oracle/run-i2p-transform.sh
+++ Oracle/run-i2p-transform.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 set -e
 
-# Expected environment variables (put there by Jenkins, etc.)
-
-# Database SID
-#export sid=
-# User and password for CDM user
-#export pcornet_cdm_user=
-#export pcornet_cdm=
-
-#export i2b2_data_schema=
-#export i2b2_meta_schema=
-#export datamart_id=
-#export datamart_name=
-#export network_id=
-#export network_name=
-# export i2b2_etl_schema=
-# export min_pat_list_date_dd_mon_rrrr=
-# export min_visit_date_dd_mon_rrrr=
-# export enrollment_months_back=36
-
-# All i2b2 terms - used for local path mapping
-#export terms_table=
-
 python load_csv.py harvest_local harvest_local.csv harvest_local.ctl pcornet_cdm_user pcornet_cdm
 python load_csv.py PMN_LabNormal pmn_labnormal.csv pmn_labnormal.ctl pcornet_cdm_user pcornet_cdm
 python load_csv.py lab_loinc_mapping lab_loinc_mapping.csv lab_loinc_mapping.ctl pcornet_cdm_user pcornet_cdm
@@ -58,11 +36,46 @@ select rxcui from "&&i2b2_etl_schema".clarity_med_id_to_rxcui@id where 1=0;
 select case when qty > 0 then 1 else 1/0 end obs_fact_meds_populated from (
   select count(*) qty from observation_fact_meds
   );
-
 EOF
 
 
-# Insert local terms as leaves of the PCORNet terms and run the transform
+########### Initialize Schema ###########
+if [ ${initialize_schema} == 'true']
+then
+
+sqlplus /nolog <<EOF
+connect ${pcornet_cdm_user}/${pcornet_cdm}
+
+set echo on;
+set timing on;
+set linesize 3000;
+set pagesize 5000;
+
+define i2b2_data_schema=${i2b2_data_schema}
+define i2b2_meta_schema=${i2b2_meta_schema}
+define i2b2_etl_schema=${i2b2_etl_schema}
+define datamart_id=${datamart_id}
+define datamart_name=${datamart_name}
+define network_id=${network_id}
+define network_name=${network_name}
+define terms_table=${terms_table}
+define min_pat_list_date_dd_mon_rrrr=${min_pat_list_date_dd_mon_rrrr}
+define min_visit_date_dd_mon_rrrr=${min_visit_date_dd_mon_rrrr}
+define enrollment_months_back=${enrollment_months_back}
+define pcornet_cdm_user=${pcornet_cdm_user}
+define start_with=${start_with}
+
+-- Prepare for transform
+start PCORNetInit.sql
+EOF
+
+fi
+
+
+########### Load Transform Procedures ###########
+if [ ${load_procedures} == 'true' ]
+then
+
 sqlplus /nolog <<EOF
 connect ${pcornet_cdm_user}/${pcornet_cdm}
 
@@ -84,8 +97,43 @@ define min_visit_date_dd_mon_rrrr=${min_visit_date_dd_mon_rrrr}
 define enrollment_months_back=${enrollment_months_back}
 define pcornet_cdm_user=${pcornet_cdm_user}
 
--- SCILHS transform
+-- Define transform procedures
 start PCORNetLoader_ora.sql
+EOF
+
+fi
+
+
+########### Exectute Transform ########### 
+if [ ${start_with} != 'PCORNetPostProc' ]
+then 
+
+sqlplus /nolog <<EOF
+connect ${pcornet_cdm_user}/${pcornet_cdm}
+
+set echo on;
+set timing on;
+set linesize 3000;
+set pagesize 5000;
+
+-- Run transform
+execute PCORNetLoader('${start_with}');
+EOF
+
+fi
+
+
+########### Run Post Proc Cleanup / Stats / Tests ###########
+sqlplus /nolog <<EOF
+connect ${pcornet_cdm_user}/${pcornet_cdm}
+
+set echo on;
+set timing on;
+set linesize 3000;
+set pagesize 5000;
+
+define pcornet_cdm_user=${pcornet_cdm_user}
+define i2b2_data_schema=${i2b2_data_schema}
 
 -- Post-process steps
 start cdm_postproc.sql
