2dc151b36203404fbc3743229bd2f4b2d80c5225
Michael Prittie <mprittie@kumc.edu>
2017-06-01 09:57:45 -0500

    Moved post proc tasks into a procedure to better control execution flow.

--- Oracle/PCORNetLoader_ora.sql
+++ Oracle/PCORNetLoader_ora.sql
@@ -1113,7 +1113,54 @@ insert into i2pReport values(v_runid, SYSDATE, 'Dispensing',	null,		dispensings,
 end pcornetReport;
 /
 
-
+--------------------------------------------------------------------------------
+-- PCORNetPostProc procedure
+--
+-- Ideally this procedure would be empty, but as a matter of practice there are
+-- often post processing cleanup tasks that needs to be complete.  Such tasks
+-- should reside here with the goal of incorporating them into to the proper
+-- procedures above.
+--------------------------------------------------------------------------------
+create or replace procedure PCORNetPostProc as
+begin
+  
+  /* Copy providerid from encounter table to diagnosis, procedures tables.
+  CDM specification says:
+    "Please note: This is a field replicated from the ENCOUNTER table."
+  */
+  merge into diagnosis d
+  using encounter e
+     on (d.encounterid = e.encounterid)
+  when matched then update set d.providerid = e.providerid;
+  
+  merge into procedures p
+  using encounter e
+     on (p.encounterid = e.encounterid)
+  when matched then update set p.providerid = e.providerid;
+  
+  merge into prescribing p
+  using encounter e
+     on (p.encounterid = e.encounterid)
+  when matched then update set p.rx_providerid = e.providerid;
+  
+  /* Currently in HERON, we have hight in cm and weight in oz (from visit vitals).
+  The CDM wants height in inches and weight in pounds. */
+  update vital v set v.ht = v.ht / 2.54;
+  update vital v set v.wt = v.wt / 16;
+  
+  /* Remove rows from the PRESCRIBING table where RX_* fields are null
+     TODO: Remove this when fixed in HERON
+   */
+  delete
+  from prescribing
+  where rx_basis is null
+    and rx_quantity is null
+    and rx_frequency is null
+    and rx_refills is null
+  ;
+  
+end PCORNetPostProc;
+/
 
 
 create or replace PROCEDURE PCORNetLoader(start_with VARCHAR2) AS
@@ -1201,5 +1248,7 @@ begin
     ) then
       PCORNetHarvest;
   end if;
+  
+  PCORNetPostProc;
 end PCORNetLoader;
 /
