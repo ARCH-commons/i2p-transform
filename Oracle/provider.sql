e8fc1fed6b65235dd81ee4ecb3f8f8756221bc02
catechol <catechol@kumc.edu>
2018-05-14 16:26:34 -0500

    Provider relies on provider_dimension rather than clarity@id reads

--- Oracle/provider.sql
+++ Oracle/provider.sql
@@ -42,18 +42,17 @@ PMN_DROPSQL('drop index provider_idx');
 execute immediate 'truncate table provider';
 
 insert into provider(providerid, provider_sex, provider_specialty_primary, provider_npi, provider_npi_flag, raw_provider_specialty_primary)
-select cs.prov_id
-  , case when cs.sex = 'U' then 'UN'
-    when cs.sex is null then 'NI'
-    else cs.sex end as sex
+select pd.provider_id
+  , case when pd.provider_sex = 'U' then 'UN'
+    when pd.provider_sex is null then 'NI'
+    else pd.provider_sex end as sex
   , case when sm.npi is null then 'NI'
     else nvl(sm.specialty, 'UN') end as provider_specialty_primary
-  , cs2.npi
-  , case when cs2.npi is not null then 'Y' else 'N' end as provider_npi_flag
+  , pd.provider_npi
+  , case when pd.provider_npi is not null then 'Y' else 'N' end as provider_npi_flag
   , substr(sp.descriptive_text, 1, 50)
-  from clarity.clarity_ser@id cs
-  left join clarity.clarity_ser_2@id cs2 on cs.prov_id = cs2.prov_id
-  left join provider_specialty_map sm on sm.npi = cs2.npi
+  from &&i2b2_data_schema.provider_dimension pd
+  left join provider_specialty_map sm on sm.npi = pd.provider_npi
   left join provider_specialty_code sp on sm.specialty = sp.code;
 
 execute immediate 'create index provider_idx on provider (PROVIDERID)';
