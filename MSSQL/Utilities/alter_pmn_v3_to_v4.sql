c65eaa85e83cbab2feb314c400975e97704bec44
Matthew Joss <mjoss@partners.org>
2018-06-14 10:54:59 -0400

    fixed some typos, added harvest table.

--- MSSQL/Utilities/alter_pmn_v3_to_v4.sql
+++ MSSQL/Utilities/alter_pmn_v3_to_v4.sql
@@ -2,9 +2,9 @@
 
 Alter Table pmndemographic add PAT_PREF_LANGUAGE_SPOKEN  varchar(3) NULL;
 
-Alter Table pmnencounter add PAYER_TYPE_PRIMARY varchar(4) NULL;
+Alter Table pmnencounter add PAYER_TYPE_PRIMARY varchar(5) NULL;
 
-Alter Table pmnencounter add PAYER_TYPE_SECONDARY varchar(4) NULL;
+Alter Table pmnencounter add PAYER_TYPE_SECONDARY varchar(5) NULL;
 
 Alter Table pmnencounter add FACILITY_TYPE varchar(50) NULL;
 
@@ -22,15 +22,15 @@ Alter Table pmnpro_cm add PRO_MEASURE_NAME varchar(50) NULL;
 
 Alter Table pmnpro_cm add PRO_MEASURE_SEQ varchar(50) NULL;
 
-Alter Table pmnpro_cm add PRO_MEASURE_SCORE varchar(8) NULL;
+Alter Table pmnpro_cm add PRO_MEASURE_SCORE numeric(15,8) NULL;
 
-Alter Table pmnpro_cm add PRO_MEASURE_THETA varchar(8) NULL;
+Alter Table pmnpro_cm add PRO_MEASURE_THETA numeric(15,8) NULL;
 
-Alter Table pmnpro_cm add PRO_MEASURE_SCALED_TSCORE varchar(8) NULL;
+Alter Table pmnpro_cm add PRO_MEASURE_SCALED_TSCORE numeric(15,8) NULL;
 
-Alter Table pmnpro_cm add PRO_MEASURE_STANDARD_ERROR varchar(8) NULL;
+Alter Table pmnpro_cm add PRO_MEASURE_STANDARD_ERROR numeric(15,8) NULL;
 
-Alter Table pmnpro_cm add PRO_MEASURE_COUNT_SCORED varchar(8) NULL;
+Alter Table pmnpro_cm add PRO_MEASURE_COUNT_SCORED numeric(15,8) NULL;
 
 Alter Table pmnpro_cm add PRO_MEASURE_LOINC varchar(10) NULL;
 
@@ -42,6 +42,8 @@ Alter Table pmnpro_cm add PRO_ITEM_TEXT varchar(50) NULL;
 
 Alter Table pmnpro_cm add PRO_MEASURE_FULLNAME varchar(50) NULL;
 
+Alter Table pmnpro_cm add PRO_RESPONSE_NUM numeric(15,8) NULL;
+
 Alter Table pmnlab_result_cm add RESULT_SNOMED varchar(50) NULL;
 
 Alter Table pmnprescribing add RX_DOSE_ORDERED numeric(15, 8) NULL;
@@ -52,7 +54,7 @@ Alter Table pmnprescribing add RX_ROUTE varchar(50) NULL;
 
 Alter Table pmnprescribing add RX_SOURCE varchar(2) NULL;
     
-Alter Table pmnprescribing add RX_DISPENSE_AS_WRITTEN varchar(50) NULL;
+Alter Table pmnprescribing add RX_DISPENSE_AS_WRITTEN varchar(2) NULL;
 
 Alter Table pmnprescribing add RX_PRN_FLAG varchar(1) NULL;
 
@@ -67,8 +69,6 @@ Alter Table pmndispensing add DISPENSE_DOSE_DISP_UNIT varchar(50) NULL;
 Alter Table pmndispensing add DISPENSE_ROUTE varchar(50) NULL;
 
 Alter Table pmnprocedures add PPX varchar(2) NULL;
-
-
 
 ------------------------------RAW FIELDS----------------------------------------
 
@@ -108,7 +108,22 @@ Alter Table pmndispensing add RAW_DISPENSE_ROUTE varchar(50) NULL;
 
 Alter Table pmnprocedures add RAW_PPX varchar(50) NULL;
 
+------------------------------HARVEST----------------------------------------
 
+Alter Table pmnharvest add DEATH_DATE_MGMT varchar(2) NULL;
 
+Alter Table pmnharvest add MEDADMIN_START_DATE_MGMT varchar(2) NULL;
 
+Alter Table pmnharvest add MEDADMIN_STOP_DATE_MGMT varchar(2) NULL;
 
+Alter Table pmnharvest add OBSCLIN_DATE_MGMT varchar(2) NULL;
+
+Alter Table pmnharvest add OBSGEN_DATE_MGMT varchar(2) NULL;
+
+Alter Table pmnharvest add REFRESH_MED_ADMIN_DATE datetime NULL;
+
+Alter Table pmnharvest add REFRESH_OBS_CLIN_DATE datetime NULL;
+
+Alter Table pmnharvest add REFRESH_OBS_GEN_DATE datetime NULL;
+
+Alter Table pmnharvest add REFRESH_PROVIDER_DATE datetime NULL; 
