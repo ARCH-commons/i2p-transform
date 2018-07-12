8339b1c22d094207e66cbec0bc67c5ef57b2ba5c
catechol <catechol@kumc.edu>
2018-07-12 16:28:00 -0500

    Adding present on admission (POA) to diagnosis.
    Heron ETL provides POA as a valueless fact when true, granular POA codes
    are not recorded.  This work will need to be revisited after updates
    are made in Heron.

--- Oracle/diagnosis.sql
+++ Oracle/diagnosis.sql
@@ -18,11 +18,13 @@ CREATE TABLE diagnosis(
 	DX_SOURCE varchar(2) NOT NULL,
     DX_ORIGIN varchar(2) NULL,
 	PDX varchar(2) NULL,
+	DX_POA varchar(2) NULL,
 	RAW_DX varchar(50) NULL,
 	RAW_DX_TYPE varchar(50) NULL,
 	RAW_DX_SOURCE varchar(50) NULL,
 	RAW_ORIGDX varchar(50) NULL,
-	RAW_PDX varchar(50) NULL
+	RAW_PDX varchar(50) NULL,
+	RAW_DX_POA varchar(50) NULL
 )
 /
 
@@ -88,6 +90,24 @@ CREATE TABLE ORIGINFACT  (
 	C_FULLNAME   	VARCHAR2(700) NOT NULL
 	)
 /
+
+BEGIN
+PMN_DROPSQL('DROP TABLE poafact');
+END;
+/
+
+CREATE TABLE POAFACT  (
+	PATIENT_NUM  	NUMBER(38) NOT NULL,
+	ENCOUNTER_NUM	NUMBER(38) NOT NULL,
+	PROVIDER_ID  	VARCHAR2(50) NOT NULL,
+	CONCEPT_CD   	VARCHAR2(50) NOT NULL,
+	START_DATE   	DATE NOT NULL,
+	POASOURCE       VARCHAR2(50) NULL,
+	RAWPOASOURCE    VARCHAR2(50) NULL,
+	C_FULLNAME   	VARCHAR2(700) NOT NULL
+	)
+/
+
 create or replace procedure PCORNetDiagnosis as
 begin
 
@@ -95,11 +115,13 @@ PMN_DROPSQL('drop index diagnosis_idx');
 PMN_DROPSQL('drop index sourcefact_idx');
 PMN_DROPSQL('drop index pdxfact_idx');
 PMN_DROPSQL('drop index originfact_idx');
+PMN_DROPSQL('drop index poafact_idx');
 
 execute immediate 'truncate table diagnosis';
 execute immediate 'truncate table sourcefact';
 execute immediate 'truncate table pdxfact';
 execute immediate 'truncate table originfact';
+execute immediate 'truncate table poafact';
 
 insert into sourcefact
 	select distinct patient_num, encounter_num, provider_id, concept_cd, start_date, dxsource.pcori_basecode dxsource, dxsource.c_fullname
@@ -131,7 +153,17 @@ insert into originfact --CDM 3.1 addition
 execute immediate 'create index originfact_idx on originfact (patient_num, encounter_num, provider_id, concept_cd, start_date)';
 GATHER_TABLE_STATS('ORIGINFACT');
 
-insert into diagnosis (patid,	encounterid, enc_type, admit_date, providerid, dx, dx_type, dx_source, dx_origin, pdx)
+insert into poafact
+	select patient_num, encounter_num, provider_id, concept_cd, start_date, 'Y' poasource, 'Yes' rawpoasource, dxsource.c_fullname
+	from i2b2fact factline
+    inner join ENCOUNTER enc on enc.patid = factline.patient_num and enc.encounterid = factline.encounter_Num
+    inner join pcornet_diag dxsource on factline.modifier_cd = dxsource.c_basecode
+	and dxsource.c_fullname like '\PCORI_MOD\DX_ORIGIN\BI\DX|BILL:POA\%';
+
+execute immediate 'create index poafact_idx on poafact (patient_num, encounter_num, provider_id, concept_cd, start_date)';
+GATHER_TABLE_STATS('POAFACT');
+
+insert into diagnosis (patid, encounterid, enc_type, admit_date, providerid, dx, dx_type, dx_source, dx_origin, pdx, dx_poa, raw_dx_poa)
 /* KUMC started billing with ICD10 on Oct 1, 2015. */
 with icd10_transition as (
   select date '2015-10-01' as cutoff from dual
@@ -213,7 +245,11 @@ select distinct factline.patient_num, factline.encounter_num encounterid,	enc_ty
     nvl(SUBSTR(originsource,INSTR(originsource, ':')+1,2),'NI') dx_origin,
 	CASE WHEN enc_type in ('EI', 'IP', 'IS', 'OS')
              THEN nvl(SUBSTR(pdxsource,INSTR(pdxsource, ':')+1,2),'NI')
-             ELSE null END PDX
+             ELSE null END PDX,
+    CASE WHEN enc_type in ('EI', 'IP')
+             THEN nvl(SUBSTR(poasource,INSTR(poasource, ':')+1,2),'UN')
+             ELSE null END DX_POA
+     , rawpoasource RAW_DX_POA
 from diag_fact_cutoff_filter factline
 inner join encounter enc on enc.patid = factline.patient_num and enc.encounterid = factline.encounter_Num
  left outer join sourcefact
@@ -234,6 +270,13 @@ and factline.encounter_num=originfact.encounter_num
 and factline.provider_id=originfact.provider_id
 and factline.concept_cd=originfact.concept_cd
 and factline.start_date=originfact.start_Date
+left outer join poafact
+on	factline.patient_num=poafact.patient_num
+and factline.encounter_num=poafact.encounter_num
+and factline.provider_id=poafact.provider_id
+and factline.concept_cd=poafact.concept_cd
+and factline.start_date=poafact.start_Date
+
 where (sourcefact.c_fullname like '\PCORI_MOD\CONDITION_OR_DX\DX_SOURCE\%' or sourcefact.c_fullname is null)
 -- order by enc.admit_date desc
 ;
