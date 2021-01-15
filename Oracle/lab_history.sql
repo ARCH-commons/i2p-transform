01e28a5344359bdf5830c7d1ab6fab9caea579a4
Sravani <sravanichandaka@gmail.com>
2021-01-15 10:06:29 -0600

    Created lab_history table

---
+++ Oracle/lab_history.sql
@@ -0,0 +1,49 @@
+insert into cdm_status (task, start_time) select 'lab_history', sysdate from dual
+/
+BEGIN
+PMN_DROPSQL('DROP SEQUENCE lab_history_seq');
+END;
+/
+create sequence  lab_history_seq cache 2000
+/
+create or replace trigger lab_history_trg
+before insert on lab_history
+for each row
+begin
+  select lab_history_seq.nextval into :new.LABHISTORYID from dual;
+end;
+/
+BEGIN
+PMN_DROPSQL('DROP TABLE lab_history');
+END;
+/
+
+CREATE TABLE lab_history(
+    LABHISTORYID varchar(50),
+    LAB_LOINC varchar(10),
+    LAB_FACILITYID varchar(50),
+    SEX varchar(2),
+    RACE varchar(2),
+    AGE_MIN_WKS varchar(2),
+    AGE_MAX_WKS varchar(2),
+    RESULT_UNIT varchar(2),
+    NORM_RANGE_LOW varchar(10),
+    NORM_MODIFIER_LOW varchar(2),
+    NORM_RANGE_HIGH varchar(2),
+    NORM_RANGE_MODIFIER_HIGH varchar(2),
+    PERIOD_START DATE,
+    PERIOD_END DATE,
+    RAW_LAB_NAME varchar(50),
+    RAW_UNIT varchar(50),
+    RAW_RANGE varchar(50)
+)
+/
+BEGIN
+GATHER_TABLE_STATS('LAB_HISTORY');
+END;
+/                  
+update cdm_status
+set end_time = sysdate, records = (select count(*) from lab_history)
+where task = 'lab_history'
+/
+
